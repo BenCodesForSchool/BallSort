412428f531cd51084a7fb24177cc27f3827861eb
harisa23 <harisahmad23@hotmail.com>
2022-04-13 13:20:27 -0400

    added setup activity

--- app/src/main/java/com/example/ballsort/BallSortActivity.java
+++ app/src/main/java/com/example/ballsort/BallSortActivity.java
@@ -1,8 +1,22 @@
 package com.example.ballsort;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
 
 public class BallSortActivity extends Activity {
 
+    final static String MYDEBUG = "MYDEBUG"; // for Log.i messages
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Log.i(MYDEBUG, "Got here! (BallSortActivity - onCreate)");
+
+        setContentView(R.layout.setup);
+    }
 }
