0f0081fd65a2363a783f5b6202bf0fd0df5a5c62
BenCodesForSchool <bjweiler92@gmail.com>
2022-04-14 12:16:10 -0400

    testing

--- app/src/main/java/com/example/ballsort/BallPanel.java
+++ app/src/main/java/com/example/ballsort/BallPanel.java
@@ -45,7 +45,7 @@ public class BallPanel extends View {
     float width, height, pixelDensity;
     int labelTextSize, statsTextSize, gap, offset;
 
-    RectF innerRectangle, outerRectangle, innerShadowRectangle, outerShadowRectangle, ballNow;
+    RectF innerRectangle, outerRectangle, innerShadowRectangle, outerShadowRectangle, ballNow, upLeft, upMid, upRight, midLeft, midRight, bLeft, bMid, bRight;
     boolean touchFlag;
     Vibrator vib;
     int wallHits;
@@ -65,7 +65,7 @@ public class BallPanel extends View {
     float dBall; // the amount to move the ball (in pixels): dBall = dT * velocity
     float xCenter, yCenter; // the center of the screen
     long now, lastT;
-    Paint statsPaint, labelPaint, linePaint, fillPaint, backgroundPaint;
+    Paint bluePaint, greenPaint, orangePaint, purplePaint, yellowPaint, pinkPaint, cyanPaint,  redPaint;
     float[] updateY;
 
     public BallPanel(Context contextArg)
@@ -88,28 +88,40 @@ public class BallPanel extends View {
 
     private void initialize(Context c)
     {
-        linePaint = new Paint();
-        linePaint.setColor(Color.RED);
-        linePaint.setStyle(Paint.Style.STROKE);
-        linePaint.setStrokeWidth(2);
-        linePaint.setAntiAlias(true);
+        redPaint = new Paint();
+        redPaint.setColor(Color.RED);
+        redPaint.setStyle(Paint.Style.FILL);
 
-        fillPaint = new Paint();
-        fillPaint.setColor(0xffccbbbb);
-        fillPaint.setStyle(Paint.Style.FILL);
+        bluePaint = new Paint();
+        bluePaint.setColor(Color.BLUE);
+        bluePaint.setStyle(Paint.Style.FILL);
 
-        backgroundPaint = new Paint();
-        backgroundPaint.setColor(Color.LTGRAY);
-        backgroundPaint.setStyle(Paint.Style.FILL);
+        greenPaint = new Paint();
+        greenPaint.setColor(Color.GREEN);
+        greenPaint.setStyle(Paint.Style.FILL);
 
-        labelPaint = new Paint();
-        labelPaint.setColor(Color.BLACK);
-        labelPaint.setTextSize(DEFAULT_LABEL_TEXT_SIZE);
-        labelPaint.setAntiAlias(true);
+        pinkPaint = new Paint();
+        pinkPaint.setColor(0xffc0cb);
+        pinkPaint.setStyle(Paint.Style.FILL);
 
-        statsPaint = new Paint();
-        statsPaint.setAntiAlias(true);
-        statsPaint.setTextSize(DEFAULT_STATS_TEXT_SIZE);
+        orangePaint = new Paint();
+        orangePaint.setColor(0xff6600);
+        orangePaint.setStyle(Paint.Style.FILL);
+
+        purplePaint = new Paint();
+        purplePaint.setColor(0x800080);
+        purplePaint.setStyle(Paint.Style.FILL);
+
+        yellowPaint = new Paint();
+        yellowPaint.setColor(Color.YELLOW);
+        yellowPaint.setStyle(Paint.Style.FILL);
+
+        cyanPaint = new Paint();
+        cyanPaint.setColor(Color.CYAN);
+        cyanPaint.setStyle(Paint.Style.FILL);
+
+
+
 
         // NOTE: we'll create the actual bitmap in onWindowFocusChanged
         //decodedBallBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
@@ -121,6 +133,14 @@ public class BallPanel extends View {
         innerRectangle = new RectF();
         innerShadowRectangle = new RectF();
         outerShadowRectangle = new RectF();
+        upLeft = new RectF();
+        upMid = new RectF();
+        upRight = new RectF();
+        midLeft = new RectF();
+        midRight = new RectF();
+        bLeft = new RectF();
+        bRight = new RectF();
+        bMid = new RectF();
         ballNow = new RectF();
         wallHits = 0;
 
@@ -134,7 +154,7 @@ public class BallPanel extends View {
 
 
         Rectangle white = new Rectangle();
-        white.setBounds(xcoord/2, ycoord/2, xcoord, ycoord);
+        white.setBounds(xcoord/2, ycoord/2, xcoord * 3/2, ycoord * 3/2);
 
     }
 
@@ -151,15 +171,56 @@ public class BallPanel extends View {
 
         // draw the paths
             // draw fills
-            canvas.drawRect(outerRectangle, fillPaint);
-            canvas.drawRect(innerRectangle, backgroundPaint);
+        upLeft.left = (float) (0.00);
+        upLeft.right = (float) (this.getWidth()/3.00);
+        upLeft.bottom = (float) (this.getHeight() - (this.getHeight()/3.00));
+        upLeft.top = (float)(this.getHeight());
+        canvas.drawRect(upLeft, bluePaint);
+
+        upMid.left = (float)(this.getWidth()/3.00);
+        upMid.bottom = (float)(this.getHeight() - (this.getHeight()/3.00));
+        upMid.top = (float)(this.getHeight());
+        upMid.right = (float) (this.getWidth() * 2.00/3.00);
+
+        upRight.left = (float)(this.getWidth() * 2.00/3.00);
+        upRight.bottom = (float)(this.getHeight() - (this.getHeight()/3.00));
+        upRight.top = (float)(this.getHeight());
+        upRight.right = (float) (this.getWidth());
+
+        midLeft.left = (float) (0.00);
+        midLeft.right = (float) (this.getWidth()/3.00);
+        midLeft.bottom = (float) (this.getHeight()/3.00);
+        midLeft.top = (float)(this.getHeight() - (this.getHeight()/3.00));
+
+        midRight.left = (float)(this.getWidth() * 2.00/3.00);
+        midRight.right = (float) (this.getWidth());
+        midRight.bottom = (float) (this.getHeight()/3.00);
+        midRight.top = (float)(this.getHeight() - (this.getHeight()/3.00));
+
+        bLeft.left = (float) (0.00);
+        bLeft.right = (float) (this.getWidth()/3.00);
+        bLeft.bottom = (float) (0.00);
+        bLeft.top = (float)(this.getHeight()/3.00);
+
+        bMid.left = (float)(this.getWidth()/3.00);
+        bMid.right = (float) (this.getWidth() * 2.00/3.00);
+        bMid.bottom = (float) (0.00);
+        bMid.top = (float)(this.getHeight()/3.00);
+
+        bRight.left = (float)(this.getWidth() * 2.00/3.00);
+        bRight.right = (float) (this.getWidth());
+        bRight.bottom = (float) (0.00);
+        bRight.top = (float)(this.getHeight()/3.00);
+
+
+
+
             Paint white = new Paint();
             white.setColor(Color.WHITE);
-            canvas.drawRoundRect(new RectF(xcoord/2, ycoord/2, xcoord, ycoord), 10, 10, white);
+            canvas.drawRoundRect(new RectF(xcoord/2, ycoord/2, xcoord * 3/2, ycoord * 3/2), 10, 10, white);
 
             // draw lines
-            canvas.drawRect(outerRectangle, linePaint);
-            canvas.drawRect(innerRectangle, linePaint);
+
 
 
         // draw the ball in its new location
